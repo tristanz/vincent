421cddb7b8a31256de9115d807b7dcd7729e0be0
Tristan Zajonc <tristanz@gmail.com>
2013-10-13 15:18:41 -0700

    Typo.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ kw = {
     'keywords': 'data visualization',
     'classifiers': classifiers,
     'packages': ['vincent'],
-    'package_data': {'vincent': ['*.html, *.js']},
+    'package_data': {'vincent': ['*.html', '*.js']},
     'install_requires': required,
     'zip_safe': True,
 }
