ae21c5be1ea88c94617bb15e67f7d48f1e59980f
Tristan Zajonc <tristanz@gmail.com>
2013-10-20 12:38:17 -0700

    Typo.

--- vincent/core.py
+++ vincent/core.py
@@ -31,7 +31,7 @@ def initialize_notebook():
         print('IPython Notebook could not be loaded.')
 
     resources = ['d3.min.js', 'topojson.js', 'd3.geo.projection.v0.min.js', 'vega.min.js']
-    for resource in resources
+    for resource in resources:
         display(JavaScript(unicode(resource_string('vincent', resource))))
     display(Javascript('$([IPython.events]).trigger("vega_loaded.vincent")'))
 
